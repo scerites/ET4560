db61921360903d11a44e7bb931c51ebb2207fa14
dangbird <danielgbird@gmail.com>
2014-06-17 20:04:17 -0600

    Added Supyo so da boys have a template

---
+++ unit1/SupYo/SupYo/talkintoya.cpp
@@ -0,0 +1,18 @@
+/*
+Dan BIrd
+Lab 1 Unit 1
+Tells you Sup Yo.
+*/
+
+
+#include <iostream> //Include all this files crap here.
+
+using namespace std;//Name the spot/space we be usin.
+
+int main()//Da Main stuff
+{
+	cout << "Dan is trying to make programming interesting and failing.." << endl;
+
+	cin.get();
+	return 0;//release used RAM To the OS so it can be Good.
+}
