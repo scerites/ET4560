0cdc89d78eef79ac1146fece791aff21a5c057c1
dangbird <danielgbird@gmail.com>
2014-07-08 17:53:49 -0600

    Unit 4 Recursion Example

---
+++ unit4/Recursion/Recursion/message.cpp
@@ -0,0 +1,28 @@
+// This program demonstrates a simple recursive function.
+#include <iostream>
+using namespace std;
+
+// Function prototype
+void message(int);
+
+int main()
+{
+	message(3);
+	cin.get();
+	return 0;
+}
+
+//***********************************************************
+// Definition of function message. If the value in times is *
+// greater than 0, the message is displayed and the         *
+// function is recursively called with the argument         *
+// times - 1.                                               *
+//***********************************************************
+void message(int times)
+{
+	if (times > 0)
+	{
+		cout << "Message called " << times << "\n";
+		message(times - 1);
+	}
+}
