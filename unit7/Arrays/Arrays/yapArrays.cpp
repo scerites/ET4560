5c774c607c982ec9fb531e9363e5da174d210b94
dangbird <danielgbird@gmail.com>
2014-07-29 20:43:17 -0600

    Lab 7 Monkeys
    TUFF LAB - PLease copy and paste then inspect how it works.

---
+++ unit7/Arrays/Arrays/yapArrays.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(void)
+{
+
+	string city[99];//DEFINED/Created Built
+	cout << "Enter the CIty: " << endl;
+	cin >> city[4];
+	cout << city[4];
+	cin.ignore();
+	cin.get();
+	return 0;
+}
