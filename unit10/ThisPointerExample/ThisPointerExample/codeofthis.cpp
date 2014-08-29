edf2889ce381e3a7367553ee2279f72b989d3757
Dan Bird <danielgbird@gmail.com>
2014-08-29 12:28:46 -0600

    Update

--- unit10/ThisPointerExample/ThisPointerExample/codeofthis.cpp
+++ unit10/ThisPointerExample/ThisPointerExample/codeofthis.cpp
@@ -32,7 +32,7 @@ private:
 
 int main(void)
 {
-	Box Box1(3.3, 1.2, 1.5);    // Declare box1
+	Box Box1(3.3, 1.2);    // Declare box1
 	Box Box2(8.5, 6.0, 2.0);    // Declare box2
 	Box BoxC;    // Declare boxC or defaults from constructor
 
